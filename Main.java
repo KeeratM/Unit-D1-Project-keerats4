344f09db8f471b88e4a9a98df2647e7fb15144b8
KeeratM <155551296+KeeratM@users.noreply.github.com>
2024-01-03 14:29:23 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,440 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    ArrayList<String> firstName = new ArrayList<String>();
+    firstName.add("Marcel");
+firstName.add("Todd");
+firstName.add("Stefan");
+firstName.add("Kaia");
+firstName.add("Gary");
+firstName.add("Bobbi");
+firstName.add("Jay-Jay");
+firstName.add("Kenny");
+firstName.add("Jovan");
+firstName.add("Gracie-Mae");
+firstName.add("Kealan");
+firstName.add("Callum");
+firstName.add("Maverick");
+firstName.add("Anisah");
+firstName.add("Ritchie");
+firstName.add("Danial");
+firstName.add("Malika");
+firstName.add("Shelbie");
+firstName.add("Henna");
+firstName.add("Kajol");
+firstName.add("Leigha");
+firstName.add("Jordanne");
+firstName.add("Karan");
+firstName.add("Libbie");
+firstName.add("Rita");
+firstName.add("Paulina");
+firstName.add("Alishba");
+firstName.add("Nichole");
+firstName.add("Joel");
+firstName.add("Myrtle");
+firstName.add("Arianne");
+firstName.add("Sheikh");
+firstName.add("Lillie-May");
+firstName.add("Mustafa");
+firstName.add("Ryder");
+firstName.add("Blade");
+firstName.add("Afsana");
+firstName.add("Wayne");
+firstName.add("Burhan");
+firstName.add("Atticus");
+firstName.add("Dafydd");
+firstName.add("Shani");
+firstName.add("Angharad");
+firstName.add("Habiba");
+firstName.add("Coby");
+firstName.add("Travis");
+firstName.add("Kristian");
+firstName.add("Nakita");
+firstName.add("Kirk");
+firstName.add("Skyla");
+firstName.add("Shauna");
+firstName.add("Abdurrahman");
+firstName.add("Wasim");
+firstName.add("Terry");
+firstName.add("Ed");
+firstName.add("Cheyenne");
+firstName.add("Kimberly");
+firstName.add("Melissa");
+firstName.add("Ilayda");
+firstName.add("Fathima");
+firstName.add("Salma");
+firstName.add("Hakim");
+firstName.add("Safaa");
+firstName.add("Regan");
+firstName.add("Maizie");
+firstName.add("Faiza");
+firstName.add("Norah");
+firstName.add("Kristopher");
+firstName.add("Elisha");
+firstName.add("Janice");
+firstName.add("Ahmet");
+firstName.add("Sofie");
+firstName.add("Natalie");
+firstName.add("Inara");
+firstName.add("Cai");
+firstName.add("Anthony");
+firstName.add("Daanyaal");
+firstName.add("Aryaan");
+firstName.add("Harley");
+firstName.add("Humza");
+firstName.add("Luka");
+firstName.add("Ranveer");
+firstName.add("Tanya");
+firstName.add("Grover");
+firstName.add("Becky");
+firstName.add("Eamonn");
+firstName.add("Walter");
+firstName.add("Rosemary");
+firstName.add("Yousef");
+firstName.add("Vinny");
+firstName.add("Trystan");
+firstName.add("Rimsha");
+firstName.add("Misha");
+firstName.add("Alison");
+firstName.add("Jamel");
+firstName.add("Thalia");
+firstName.add("Jacob");
+firstName.add("Krista");
+firstName.add("Mari");
+firstName.add("Vinay");
+    ArrayList<String> lastName = new ArrayList<String>();
+    lastName.add("Green");
+lastName.add("Muir");
+lastName.add("Pratt");
+lastName.add("Bull");
+lastName.add("Rossi");
+lastName.add("Aguilar");
+lastName.add("Stephens");
+lastName.add("Barton");
+lastName.add("Iles");
+lastName.add("Roche");
+lastName.add("Smyth");
+lastName.add("Mata");
+lastName.add("Herman");
+lastName.add("Steele");
+lastName.add("Sawyer");
+lastName.add("Wolf");
+lastName.add("Francis");
+lastName.add("Mccartney");
+lastName.add("Downes");
+lastName.add("Decker");
+lastName.add("Pruitt");
+lastName.add("Soto");
+lastName.add("Mueller");
+lastName.add("Mcguire");
+lastName.add("Stephenson");
+lastName.add("Redfern");
+lastName.add("Cairns");
+lastName.add("Gross");
+lastName.add("Sosa");
+lastName.add("Chester");
+lastName.add("Roberson");
+lastName.add("Weeks");
+lastName.add("Flowers");
+lastName.add("Benitez");
+lastName.add("Vu");
+lastName.add("Woods");
+lastName.add("Merritt");
+lastName.add("Manning");
+lastName.add("Booker");
+lastName.add("Pearson");
+lastName.add("Mccallum");
+lastName.add("Bowen");
+lastName.add("Spears");
+lastName.add("Bartlett");
+lastName.add("Velez");
+lastName.add("Barlow");
+lastName.add("Greenaway");
+lastName.add("Armstrong");
+lastName.add("Freeman");
+lastName.add("Rosa");
+lastName.add("Sims");
+lastName.add("Dunne");
+lastName.add("Reyna");
+lastName.add("Bowden");
+lastName.add("Mac");
+lastName.add("Kearns");
+lastName.add("Lozano");
+lastName.add("Lott");
+lastName.add("Bates");
+lastName.add("Mcculloch");
+lastName.add("Beck");
+lastName.add("Newman");
+lastName.add("Bernard");
+lastName.add("Burris");
+lastName.add("Whyte");
+lastName.add("Guy");
+lastName.add("Choi");
+lastName.add("Blair");
+lastName.add("Logan");
+lastName.add("Lane");
+lastName.add("Erickson");
+lastName.add("Knott");
+lastName.add("Aldred");
+lastName.add("Naylor");
+lastName.add("Clarke");
+lastName.add("Smith");
+lastName.add("Moon");
+lastName.add("Mcmanus");
+lastName.add("Wilde");
+lastName.add("Hamer");
+lastName.add("Maddox");
+lastName.add("Pena");
+lastName.add("Rose");
+lastName.add("Hutchinson");
+lastName.add("Mansell");
+lastName.add("Barnard");
+lastName.add("Lugo");
+lastName.add("Ryan");
+lastName.add("Donnelly");
+lastName.add("Noble");
+lastName.add("Mercer");
+lastName.add("Olson");
+lastName.add("Villanueva");
+lastName.add("Wilson");
+lastName.add("Wilkerson");
+lastName.add("Head");
+lastName.add("Hewitt");
+lastName.add("Thomas");
+lastName.add("Gilbert");
+lastName.add("Hayden");
+    ArrayList<String> fallGrade = new ArrayList<String>();
+    fallGrade.add("A");
+fallGrade.add("B");
+fallGrade.add("A");
+fallGrade.add("C");
+fallGrade.add("D");
+fallGrade.add("F");
+fallGrade.add("C");
+fallGrade.add("B");
+fallGrade.add("A");
+fallGrade.add("F");
+fallGrade.add("B");
+fallGrade.add("C");
+fallGrade.add("A");
+fallGrade.add("A");
+fallGrade.add("B");
+fallGrade.add("B");
+fallGrade.add("B");
+fallGrade.add("A");
+fallGrade.add("B");
+fallGrade.add("A");
+fallGrade.add("C");
+fallGrade.add("D");
+fallGrade.add("F");
+fallGrade.add("C");
+fallGrade.add("B");
+fallGrade.add("A");
+fallGrade.add("F");
+fallGrade.add("B");
+fallGrade.add("F");
+fallGrade.add("C");
+fallGrade.add("B");
+fallGrade.add("A");
+fallGrade.add("F");
+fallGrade.add("B");
+fallGrade.add("C");
+fallGrade.add("A");
+fallGrade.add("A");
+fallGrade.add("B");
+fallGrade.add("B");
+fallGrade.add("B");
+fallGrade.add("A");
+fallGrade.add("B");
+fallGrade.add("A");
+fallGrade.add("C");
+fallGrade.add("D");
+fallGrade.add("F");
+fallGrade.add("C");
+fallGrade.add("B");
+fallGrade.add("B");
+fallGrade.add("A");
+fallGrade.add("F");
+fallGrade.add("B");
+fallGrade.add("C");
+fallGrade.add("A");
+fallGrade.add("A");
+fallGrade.add("B");
+fallGrade.add("B");
+fallGrade.add("B");
+fallGrade.add("A");
+fallGrade.add("B");
+fallGrade.add("A");
+fallGrade.add("B");
+fallGrade.add("A");
+fallGrade.add("C");
+fallGrade.add("D");
+fallGrade.add("F");
+fallGrade.add("C");
+fallGrade.add("B");
+fallGrade.add("A");
+fallGrade.add("F");
+fallGrade.add("B");
+fallGrade.add("C");
+fallGrade.add("A");
+fallGrade.add("A");
+fallGrade.add("B");
+fallGrade.add("B");
+fallGrade.add("B");
+fallGrade.add("A");
+fallGrade.add("B");
+fallGrade.add("A");
+fallGrade.add("C");
+fallGrade.add("D");
+fallGrade.add("F");
+fallGrade.add("B");
+fallGrade.add("C");
+fallGrade.add("A");
+fallGrade.add("A");
+fallGrade.add("B");
+fallGrade.add("B");
+fallGrade.add("B");
+fallGrade.add("A");
+fallGrade.add("B");
+fallGrade.add("A");
+fallGrade.add("C");
+fallGrade.add("D");
+fallGrade.add("A");
+fallGrade.add("A");
+fallGrade.add("B");
+fallGrade.add("B");
+fallGrade.add("B");
+    Scanner scan = new Scanner(System.in);
+    int choice;
+
+    do {
+      System.out.println("Please select a number 1-8 to apply the action writen!");
+      System.out.println("Menu:");
+      System.out.println("1. Add name and fallGrade");
+      System.out.println("2. Print list of names and current Grades");
+      System.out.println("3. Search for a name in the program");
+      System.out.println("4. Sort by last name");
+      System.out.println("5. Enter springGrades for all students");
+      System.out.println("6. Search for a student by name and alter a fallGrade");
+      System.out.println("7. Delete a student from the program");
+      System.out.println("8. Exit");
+      System.out.println("Enter your choice: ");
+      choice = Integer.parseInt(scan.nextLine());
+      switch (choice) {
+        case 1:
+          System.out.println("Enter the first name: ");
+          String newFirstName = scan.nextLine();
+          System.out.println("Enter the last name: ");
+          String newLastName = scan.nextLine();
+          System.out.println("Enter the fallGrade: ");
+          String newfallGrade = scan.nextLine();
+          firstName.add(newFirstName);
+          lastName.add(newLastName);
+          fallGrade.add(newfallGrade);
+          System.out.println("Student successfully added. Please print out names to confirm");
+          break;
+        case 2:
+          System.out.println("List of names and current Grades:");
+          for (int i = 0; i < firstName.size(); i++) {
+            System.out.println(firstName.get(i) + " " + lastName.get(i) + " - " + fallGrade.get(i));
+          }
+          break;
+        case 3:
+          System.out.println("Enter the name to search: ");
+          String searchName = scan.nextLine();
+          int index = -1;
+          for (int i = 0; i < firstName.size(); i++) {
+            if (firstName.get(i).equalsIgnoreCase(searchName) || lastName.get(i).equalsIgnoreCase(searchName)) {
+              index = i;
+              break;
+            }
+          }
+          if (index != -1) {
+            System.out.println("The name " + searchName + " is found at index " + index);
+          } else {
+            System.out.println("The name " + searchName + " is not found.");
+          }
+          break;
+        case 4:
+          selectionSort(lastName, firstName, fallGrade);
+          break;
+        case 5:
+          System.out.println("Enter springGrades for all students:");
+          for (int i = 0; i < firstName.size(); i++) {
+            System.out.println("Enter the springGrade for " + firstName.get(i) + " " + lastName.get(i) + ": ");
+            String springfallGrade = scan.nextLine();
+            fallGrade.set(i, springfallGrade);
+          }
+          break;
+        case 6:
+          System.out.println("Enter the name to search: ");
+          String searchNameToUpdate = scan.nextLine();
+          int updateIndex = -1;
+          for (int i = 0; i < firstName.size(); i++) {
+            if (firstName.get(i).equalsIgnoreCase(searchNameToUpdate) || lastName.get(i).equalsIgnoreCase(searchNameToUpdate)) {
+              updateIndex = i;
+              break;
+            }
+          }
+          if (updateIndex != -1) {
+            System.out.println("Enter the new fallGrade for " + firstName.get(updateIndex) + " " + lastName.get(updateIndex) + ": ");
+            String newfallGradeToUpdate = scan.nextLine();
+            fallGrade.set(updateIndex, newfallGradeToUpdate);
+            System.out.println("fallGrade successfully updated .Please print out names to confirm");
+          } else {
+            System.out.println("The name " + searchNameToUpdate + " is not found.");
+          }
+          break;
+
+        case 7:
+          System.out.println("Enter the name to delete: ");
+          String deleteName = scan.nextLine();
+          int deleteIndex = -1;
+          for (int i = 0; i < firstName.size(); i++) {
+            if (firstName.get(i).equalsIgnoreCase(deleteName) || lastName.get(i).equalsIgnoreCase(deleteName)) {
+              deleteIndex = i;
+              break;
+            }
+          }
+          if (deleteIndex != -1) {
+            firstName.remove(deleteIndex);
+            lastName.remove(deleteIndex);
+            fallGrade.remove(deleteIndex);
+            System.out.println("Student successfully removed. Please print out names to confirm");
+          } else {
+            System.out.println("The name " + deleteName + " is not found.");
+          }
+          break;
+        case 8:
+          System.out.println("Exiting the program.");
+          break;
+        default:
+          System.out.println("Invalid choice. Please try again.");
+          break;
+      }
+    } while (choice != 8);
+  }
+  public static void selectionSort(ArrayList<String> lastName, ArrayList<String> firstName, ArrayList<String> fallGrade) {
+    int n = lastName.size();
+    for (int i = 0; i < n - 1; i++) {
+      int minIndex = i;
+      for (int j = i + 1; j < n; j++) {
+        if (lastName.get(j).compareTo(lastName.get(minIndex)) < 0) {
+          minIndex = j;
+        }
+      }
+      if (minIndex != i) {
+        String tempLastName = lastName.get(i);
+        String tempFirstName = firstName.get(i);
+        String tempfallGrade = fallGrade.get(i);
+        lastName.set(i, lastName.get(minIndex));
+        firstName.set(i, firstName.get(minIndex));
+        fallGrade.set(i, fallGrade.get(minIndex));
+        lastName.set(minIndex, tempLastName);
+        firstName.set(minIndex, tempFirstName);
+        fallGrade.set(minIndex, tempfallGrade);
+      }
+    }
+  }
+}
